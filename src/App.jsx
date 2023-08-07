3b4fd9973e73508db3600646610f2519c9e726e8
nachospreafico <nachospreafico06@gmail.com>
2023-08-07 14:28:53 +0200

    changes to styles for > 1440px devices (desktop)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import FormContainer from "./components/FormContainer";
@@ -11,6 +11,25 @@ function App() {
     email: { value: "", isEmpty: true },
     phone: { value: "", isEmpty: true },
   });
+
+  const [isMobileScreen, setIsMobileScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileScreen(window.innerWidth < 480);
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   function handleStepIncrease() {
     if (
@@ -35,29 +54,54 @@ function App() {
       <div className="app-container">
         <div className="counter-wrapper">
           <div className="step-counter">
-            <div className={`step ${step === 1 ? "current" : ""}`}>1</div>
-            <div className={`step ${step === 2 ? "current" : ""}`}>2</div>
-            <div className={`step ${step === 3 ? "current" : ""}`}>3</div>
-            <div className={`step ${step === 4 ? "current" : ""}`}>4</div>
+            {isMobileScreen ? (
+              <>
+                <div className={`step ${step === 1 ? "current" : ""}`}>1</div>
+                <div className={`step ${step === 2 ? "current" : ""}`}>2</div>
+                <div className={`step ${step === 3 ? "current" : ""}`}>3</div>
+                <div className={`step ${step === 4 ? "current" : ""}`}>4</div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <div className={`step ${step === 1 ? "current" : ""}`}>1</div>
+                  <p>YOUR INFO</p>
+                </div>
+                <div>
+                  <div className={`step ${step === 2 ? "current" : ""}`}>2</div>
+                  <p>SELECT PLAN</p>
+                </div>
+                <div>
+                  <div className={`step ${step === 3 ? "current" : ""}`}>3</div>
+                  <p>ADD-ONS</p>
+                </div>
+                <div>
+                  <div className={`step ${step === 4 ? "current" : ""}`}>4</div>
+                  <p>SUMMARY</p>
+                </div>
+              </>
+            )}
           </div>
         </div>
-        <div className="form-wrapper">
-          <FormContainer step={step} input={input} setInput={setInput} />
-        </div>
-        <div className="buttons-container">
-          <button
-            className={`back-btn ${step === 1 || step === 5 ? "hidden" : ""}`}
-            onClick={handleStepDecrease}
-            disabled={step === 1 ? true : false}
-          >
-            Go back
-          </button>
-
-          {step < 5 ? (
-            <button className="next-btn" onClick={handleStepIncrease}>
-              {step === 4 ? "Confirm" : "Next Step"}
+        <div className="form-btn-container">
+          <div className="form-wrapper">
+            <FormContainer step={step} input={input} setInput={setInput} />
+          </div>
+          <div className="buttons-container">
+            <button
+              className={`back-btn ${step === 1 || step === 5 ? "hidden" : ""}`}
+              onClick={handleStepDecrease}
+              disabled={step === 1 ? true : false}
+            >
+              Go back
             </button>
-          ) : null}
+
+            {step < 5 ? (
+              <button className="next-btn" onClick={handleStepIncrease}>
+                {step === 4 ? "Confirm" : "Next Step"}
+              </button>
+            ) : null}
+          </div>
         </div>
       </div>
     </>
--- src/components/FormStep2.jsx
+++ src/components/FormStep2.jsx
@@ -13,71 +13,77 @@ const FormStep2 = ({ monthly, setMonthly, setPlan, planId }) => {
     <div id="step-2">
       <h1>Select your plan</h1>
       <p>You have the option of monthly or yearly billing.</p>
-      <div
-        className={`billing-option-container ${planId === 1 ? "selected" : ""}`}
-        onClick={() => {
-          console.log("planId: ", planId);
-          setPlan({ id: 1, price: 9 });
-        }}
-      >
-        <img src={Arcade} className="plan-img"></img>
-        <div className="billing-option">
-          <h3>Arcade</h3>
-          {monthly ? (
-            <p>$9/mo</p>
-          ) : (
-            <>
-              <p>$90/yr</p>
-              <p className="months-free">2 months free</p>
-            </>
-          )}
+      <div className="billing-options-wrapper">
+        <div
+          className={`billing-option-container ${
+            planId === 1 ? "selected" : ""
+          }`}
+          onClick={() => {
+            console.log("planId: ", planId);
+            setPlan({ id: 1, price: 9 });
+          }}
+        >
+          <img src={Arcade} className="plan-img"></img>
+
+          <div className="billing-option">
+            <h3>Arcade</h3>
+            {monthly ? (
+              <p>$9/mo</p>
+            ) : (
+              <>
+                <p>$90/yr</p>
+                <p className="months-free">2 months free</p>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-      <div
-        className={`billing-option-container ${planId === 2 ? "selected" : ""}`}
-        onClick={() => {
-          setPlan({ id: 2, price: 12 });
-        }}
-      >
-        <img src={Advanced} className="plan-img"></img>
-        <div className="billing-option">
-          <h3>Advanced</h3>
-          {monthly ? (
-            <p>$12/mo</p>
-          ) : (
-            <>
-              <p>$120/yr</p>
-              <p className="months-free">2 months free</p>
-            </>
-          )}
+        <div
+          className={`billing-option-container ${
+            planId === 2 ? "selected" : ""
+          }`}
+          onClick={() => {
+            setPlan({ id: 2, price: 12 });
+          }}
+        >
+          <img src={Advanced} className="plan-img"></img>
+          <div className="billing-option">
+            <h3>Advanced</h3>
+            {monthly ? (
+              <p>$12/mo</p>
+            ) : (
+              <>
+                <p>$120/yr</p>
+                <p className="months-free">2 months free</p>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-      <div
-        className={`billing-option-container ${planId === 3 ? "selected" : ""}`}
-        onClick={() => {
-          setPlan({ id: 3, price: 15 });
-        }}
-      >
-        <img src={Pro} className="plan-img"></img>
-        <div className="billing-option">
-          <h3>Pro</h3>
-          {monthly ? (
-            <p>$15/mo</p>
-          ) : (
-            <>
-              <p>$150/yr</p>
-              <p className="months-free">2 months free</p>
-            </>
-          )}
+        <div
+          className={`billing-option-container ${
+            planId === 3 ? "selected" : ""
+          }`}
+          onClick={() => {
+            setPlan({ id: 3, price: 15 });
+          }}
+        >
+          <img src={Pro} className="plan-img"></img>
+          <div className="billing-option">
+            <h3>Pro</h3>
+            {monthly ? (
+              <p>$15/mo</p>
+            ) : (
+              <>
+                <p>$150/yr</p>
+                <p className="months-free">2 months free</p>
+              </>
+            )}
+          </div>
         </div>
       </div>
       <div className="billing-slider">
         <p className="slider-monthly">Monthly</p>
-        <div className="slider-container">
-          <div
-            className={`slider-${monthly ? "left" : "right"}`}
-            onClick={handleBillingOption}
-          ></div>
+        <div className="slider-container" onClick={handleBillingOption}>
+          <div className={`slider-${monthly ? "left" : "right"}`}></div>
         </div>
         <p className="slider-yearly">Yearly</p>
       </div>
